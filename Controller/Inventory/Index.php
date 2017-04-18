9b25d49535143ebe446dfbaaa89c12022c4319b6
Robbie Averill <robbie@silverstripe.com>
2017-04-18 19:37:15 +1200

    Add more error handling to inventory action, fix helper call to isConfigurable, update Magento route

--- Controller/Inventory/Index.php
+++ Controller/Inventory/Index.php
@@ -25,12 +25,14 @@ class Index extends \Magento\Framework\App\Action\Action
     protected $productRepository;
 
     /**
+     * @param \Magento\Framework\App\Action\Context            $context
      * @param \Magento\Framework\Controller\Result\JsonFactory $jsonFactory
-     * @param \AltoLabs\Snappic\Helper\Data $snappicHelper
-     * @param \Magento\Framework\Json\Helper\Data $jsonHelper
-     * @param \Magento\Catalog\Api\ProductRepositoryInterface $productRepository
+     * @param \AltoLabs\Snappic\Helper\Data                    $snappicHelper
+     * @param \Magento\Framework\Json\Helper\Data              $jsonHelper
+     * @param \Magento\Catalog\Api\ProductRepositoryInterface  $productRepository
      */
     public function __construct(
+        \Magento\Framework\App\Action\Context $context,
         \Magento\Framework\Controller\Result\JsonFactory $jsonFactory,
         \AltoLabs\Snappic\Helper\Data $snappicHelper,
         \Magento\Framework\Json\Helper\Data $jsonHelper,
@@ -49,15 +51,39 @@ class Index extends \Magento\Framework\App\Action\Action
      */
     public function execute()
     {
-        $payload = $this->jsonHelper->jsonDecode($this->getRequest()->getRawBody());
+        try {
+            $payload = $this->jsonHelper->jsonDecode($this->getRequest()->getContent());
+        } catch (\Zend_Json_Exception $e) {
+            return $this->jsonFactory->create()->setData([
+                'error' => 'The request was not valid JSON: ' . $e->getMessage(),
+                'quantities' => 0
+            ]);
+        }
+
+        if (empty($payload['ids'])) {
+            return $this->jsonFactory->create()->setData([
+                'error' => 'No product IDs passed.',
+                'quantities' => 0
+            ]);
+        }
+
         $ids = $payload['ids'];
         $quantities = [];
         foreach ($ids as $id) {
-            /** @var \Magento\Catalog\Api\Data\ProductInterface $product */
-            $product = $this->productRepository->getById($id);
+            try {
+                /** @var \Magento\Catalog\Api\Data\ProductInterface $product */
+                $product = $this->productRepository->getById($id);
+            } catch (\Magento\Framework\Exception\NoSuchEntityException $e) {
+                // Note: should we just return zero here when the product doesn't exist?
+                return $this->jsonFactory->create()->setData([
+                    'error' => 'Product #' . $id . ' does not exist.',
+                    'quantities' => 0
+                ]);
+            }
             $quantities[$id] = $this->snappicHelper->getProductStock($product);
         }
-        return $this->jsonFactory->create([
+
+        return $this->jsonFactory->create()->setData([
             'status' => 'success',
             'quantities' => $quantities
         ]);
