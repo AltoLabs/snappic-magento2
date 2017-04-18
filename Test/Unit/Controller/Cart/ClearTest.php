2f6ce720adcf94072ab4770bc223cc84d573aa1e
Robbie Averill <robbie@silverstripe.com>
2017-04-19 00:40:45 +1200

    Mark all incomplete test classes as incomplete

--- Test/Unit/Controller/Cart/ClearTest.php
+++ Test/Unit/Controller/Cart/ClearTest.php
@@ -4,5 +4,8 @@ namespace AltoLabs\Snappic\Test\Unit\Controller\Cart;
 
 class ClearTest extends \PHPUnit_Framework_TestCase
 {
-
+    public function testTbc()
+    {
+        $this->markTestIncomplete('TBC');
+    }
 }
--- Test/Unit/Controller/Cart/TotalTest.php
+++ Test/Unit/Controller/Cart/TotalTest.php
@@ -4,5 +4,8 @@ namespace AltoLabs\Snappic\Test\Unit\Controller\Cart;
 
 class TotalTest extends \PHPUnit_Framework_TestCase
 {
-
+    public function testTbc()
+    {
+        $this->markTestIncomplete('TBC');
+    }
 }
